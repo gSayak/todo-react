5acffad55ce10e4013524744c96bb0361664cf27
gSayak <gsayak22@gmail.com>
2023-08-26 00:02:05 +0530

    learning about props and useState

--- src/App.js
+++ src/App.js
@@ -3,30 +3,38 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Footer from "./components/Footer";
 import Todos from "./components/Todos";
+import { useState } from "react";
 
 
 function App() {
-  let todos = [
+  const onDelete = (todo) =>{
+    console.log("I am delete of todo", todo);
+    setTodos(todos.filter((e) => {
+      return e!==todo;
+    }))
+  }
+  const[todos, setTodos] = useState([
     {
       sno: 1,
       title: "Go to market",
-      desc: "You need to go to market to get the job 1 done",
+      desc: "You need to go to market to get the job 1 done"
     },
     {
       sno: 2,
       title: "Go to ghat",
-      desc: "You need to go to ghat to get the job 2 done",
+      desc: "You need to go to ghat to get the job 2 done"
     },
     {
-      sno: 1,
+      sno: 3,
       title: "Go to house",
-      desc: "You need to go to house to get the job 3 done",
+      desc: "You need to go to house to get the job 3 done"
     },
-  ];
+  ]);
+
   return (
     <>
       <Header title="My Todo List" />
-      <Todos todos ={todos}/>
+      <Todos todos ={todos} onDelete={onDelete}/>
       <Footer />
     </>
   );
--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -4,8 +4,11 @@ import {TodoItem} from './TodoItem'
 const Todos = (props) => {
   return (
     <div className='container'>
-        <h3>Todos List</h3>
-        <TodoItem todo={props.todos[0]}/>
+        <h3 className='text-center my-3'>Todos List</h3>
+        {props.todos.map((todo) => {
+            return <TodoItem todo={todo} key={todo.sno} onDelete={props.onDelete}/>
+        })}
+        
     </div>
   )
 }
